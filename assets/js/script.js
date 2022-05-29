eb221c32a005f5cd16739a1652e10a9bd01c1a4b
viviana rodriguez <vivianaprodriguez@live.com>
2022-05-28 21:56:57 -0400

    fixed taskinator repo

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+console.log(window.document);
+
+var buttonEl = document.querySelector("#save-task");
+console.log(buttonEl);
+
+window.document.querySelector("button");
+var btn = window.document.querySelector("button");
+console.dir(btn);
+
+document.querySelector(".btn");
+
+document.querySelector("button").textContent;
+
+document.querySelector("#save-task");
+
+document.createElement("li");
+
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tas
+
+buttonEl.addEventListener("click", createTaskHandler);{
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+});
+
+//TEST
